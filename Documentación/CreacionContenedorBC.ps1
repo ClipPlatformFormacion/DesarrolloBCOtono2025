be640877832585cf873a2358c7d6c170cd072d92
Cristina Nicolàs <cristina.nicolas@clipplatform.com>
2025-10-22 12:50:37 +0000

    Documentación

---
+++ Documentación/CreacionContenedorBC.ps1
@@ -0,0 +1,5 @@
+﻿#Install-Module -Name BcContainerHelper -force
+
+$artifactUrl = Get-BCArtifactUrl -country es -select Latest -type Sandbox -version 26.5
+
+New-BcContainer -accept_eula -artifactUrl $artifactUrl -auth UserPassword -containerName BC26CU5ES -includeTestLibrariesOnly -includeTestToolkit -updateHosts -useBestContainerOS
